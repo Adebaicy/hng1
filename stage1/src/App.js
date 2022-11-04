6f09f7f38453054371864c9f11b39c59ca6e229e
Adebaicy <adebaice@gmail.com>
2022-11-04 19:26:16 +0100

    fixed the web version of the contact form

--- stage1/src/App.js
+++ stage1/src/App.js
@@ -1,109 +1,249 @@
-import React from 'react';
-import slack from './img/slack.jpg';
-import git from './img/Iconm.png';
-import share from './img/share.jpg';
-import dism from './img/dism.png';
-import focm from './img/focm.png';
-import i4g from './img/I4G.png';
-import zuri from './img/Vector.png';
-import pressm from './img/pressm.png';
-import more from './img/Icon.png';
-import profile_img from './img/profile__img.png';
-import "./App.css"
-function Cir(){
-  return(<div className="cir"><img  src = {share} alt="" />
-  <img  src = {more} alt="" /></div>);
-}
-
-function Hor(){
-  return(<div className='hor'><hr /></div>);
-}
-
-
-function Social(){
-  return (
-    <div id="social" >
-  <img src = {slack} alt="" />
-  <img src = {git} alt="" />
+import React from "react";
+import Contact from "./contact";
+import slack from "./img/slack.jpg";
+import git from "./img/Iconm.png";
+import share from "./img/share.jpg";
+import dism from "./img/dism.png";
+import focm from "./img/focm.png";
+import i4g from "./img/I4G.png";
+import zuri from "./img/Vector.png";
+import pressm from "./img/pressm.png";
+import more from "./img/Icon.png";
+import profile_img from "./img/profile__img.png";
+import { ReactDOM } from "react";
+import "./App.css";
+import {
+  Navigate,
+  BrowserRouter,
   
-  </div>);
-}
+  Route,
+  Link,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 
-function Profile(){
+function Cir() {
   return (
-    <div id="profile_img">
-  <img src = {profile_img} alt="" />
-
-  </div>);
-}
-function Profilename() {
-return(
-
-  <div id = "twitter"> Adejumo Oluwapelumi </div>
-);
-} 
-
-function Twitter () {
-  return (<button><a target='_blank' rel="noreferrer" href="twitter.com/Adebaicy"> Twitter Link</a></button>);
-}
-
-function Slack(){
-  return(
-    <div className="invisible" id="slack">Adebaicy</div>
+    <div className="cir">
+      <img src={share} alt="" />
+      <img src={more} alt="" />
+    </div>
   );
 }
-function Zurit () {
+
+function Hor() {
   return (
-  <button><a id = "btn__zuri" target='_blank' rel="noreferrer" href="https://training.zuri.team">Zuri Team</a></button>);
+    <div className="hor">
+      <hr />
+    </div>
+  );
 }
 
-function Zurib () {
-  return (<button><a id="books" target='_blank' rel="noreferrer"  href="https://books.zuri.team">Zuri Books</a></button>);
+function Social() {
+  return (
+    <div id="social">
+      <img src={slack} alt="" />
+      <img src={git} alt="" />
+    </div>
+  );
 }
 
-function Python () {
-  return (<button><a id="book__python" target='_blank' rel="noreferrer"  href="https://books.zuri.team/python-for-beginners?ref_id=Adebaicy"> Python Books </a></button>);
+function Profile() {
+  return (
+    <div id="profile_img">
+      <img src={profile_img} alt="" />
+    </div>
+  );
+}
+function Profilename() {
+  return <div id="twitter"> Adejumo Oluwapelumi </div>;
 }
 
-
-function Background () {
-  return (<button><a id="pitch" target='_blank' rel="noreferrer"  href="https://background.zuri.team"> Background Check for Coders</a></button>);
+function Twitter() {
+  return (
+    <button>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://twitter.com/Adebaicy?t=M-q627HxLvpXcqYfmWZ0Gg&s=09"
+      >
+        {" "}
+        Twitter Link
+      </a>
+    </button>
+  );
 }
 
-function Design () {
-  return (<button><a id="book__design" target='_blank' rel="noreferrer"  href="https://books.zuri.team/design-rules"> Design Books</a></button>);
+function Slack() {
+  return (
+    <div className="invisible" id="slack">
+      Adebaicy
+    </div>
+  );
+}
+function Zurit() {
+  return (
+    <button>
+      <a
+        id="btn__zuri"
+        target="_blank"
+        rel="noreferrer"
+        href="https://training.zuri.team"
+      >
+        Zuri Team
+      </a>
+    </button>
+  );
 }
 
-function Footer(){
-  return(<div className='footer'><img src = {zuri} alt="" /><small>HNG Internship 9 Frontend task</small><img src = {i4g} alt="" /></div>);
+function Zurib() {
+  return (
+    <button>
+      <a
+        id="books"
+        target="_blank"
+        rel="noreferrer"
+        href="https://books.zuri.team"
+      >
+        Zuri Books
+      </a>
+    </button>
+  );
+}
+
+function Python() {
+  return (
+    <button>
+      <a
+        id="book__python"
+        target="_blank"
+        rel="noreferrer"
+        href="https://books.zuri.team/python-for-beginners?ref_id=Adebaicy"
+      >
+        {" "}
+        Python Books{" "}
+      </a>
+    </button>
+  );
+}
+
+function Background() {
+  return (
+    <button>
+      <a
+        id="pitch"
+        target="_blank"
+        rel="noreferrer"
+        href="https://background.zuri.team"
+      >
+        {" "}
+        Background Check for Coders
+      </a>
+    </button>
+  );
+}
+
+function Design() {
+  return (
+    <button>
+      <a
+        id="book__design"
+        target="_blank"
+        rel="noreferrer"
+        href="https://books.zuri.team/design-rules"
+      >
+        {" "}
+        Design Books
+      </a>
+    </button>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <img src={zuri} alt="" />
+      <small>HNG Internship 9 Frontend task</small>
+      <img src={i4g} alt="" />
+    </div>
+  );
+}
+
+function Oldq() {
+
+  return(<div>
+    <Cir />
+      <Profile />
+
+      <Profilename />
+
+      <Slack />
+      <div className="send d-flex flex-column">
+        <Twitter />
+        <br />
+        <Zurit />
+        <br />
+        <Zurib />
+        <br />
+        <Python />
+        <br />
+        <Background />
+        <br />
+        <Design />
+        <br/>
+        <button ><NavLink to="/contact">Contact Me</NavLink></button>
+      </div>
+      
+
+    
+
+      <Social />
+      <Hor />
+      <Footer />
+      
+    
+  </div>)
 }
 function App() {
   return (
     <div className="App">
-      <Cir />
+  {/*    <Cir />
       <Profile />
-      
-     <Profilename />
- 
-     
-     <Slack />
-     <div className='send d-flex flex-column'>
-     <Twitter />
-     <br />
-     <Zurit />
-     <br />
-     <Zurib />
-     <br />
-     <Python />
-     <br />
-     <Background />
-     <br />
-     <Design />
-     </div>
-     <Social />
-     <Hor />
-     <Footer />
 
+      <Profilename />
+
+      <Slack />
+      <div className="send d-flex flex-column">
+        <Twitter />
+        <br />
+        <Zurit />
+        <br />
+        <Zurib />
+        <br />
+        <Python />
+        <br />
+        <Background />
+        <br />
+        <Design />
+      </div>
+      
+
+    
+
+      <Social />
+      <Hor />
+      <Footer />
+      
+    
+  */}
+
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Oldq />} />
+      <Route path="/contact" element={<Contact />} />
+
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
--- stage1/src/index.js
+++ stage1/src/index.js
@@ -5,13 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
